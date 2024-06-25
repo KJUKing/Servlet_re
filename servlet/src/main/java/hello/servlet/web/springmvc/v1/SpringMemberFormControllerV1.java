4e67d7df1671752354d8bae12a6694c77b4576a1
KJUKing <kyungjurhdqn@gmail.com>
2024-06-25 16:08:40 +0900

    servlet upload

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.springmvc.v1;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberFormControllerV1 {
+    @RequestMapping("/springmvc/v1/members/new-form")
+
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
+
+//@Bean
+//SpringMemberFormControllerV1 springMemberFormControllerV1(){
+//    return new SpringMemberFormControllerV1();
+//}
+
